0200b7eebef2d9a5f622cb619adc13cff7ec6cb1
Tyler Keung (18TK31) <18tk31@queensu.ca>
2024-02-26 10:45:35 -0500

    Datapath and Multi changes

--- Datapath.v
+++ Datapath.v
@@ -41,20 +41,23 @@ module Datapath(	 input R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9
 							Gen_Reg HI(busInHI, bus, HIin, clock, clear);
 							Gen_Reg LO(busInLO, bus, LOin, clock, clear);
 							Y_Reg Y(clock, clear, Mdatain, Yin,YData);
-							Z_Reg Z(busInZ, ZReg, Zin, ZLowout, ZHighout, clock, clear);
+							Z_Reg Z(busOutZ, ZReg, Zin, ZLowout, ZHighout, clear, clock);
 							
 
-							RCAdder32 add(A, BusMuxOut, zregin);
-							Gen_Reg RZ(clear, clock, Rzin, zregin, BusMuxInRz);
+							//Gen_Reg RZ(clear, clock, Rzin, zregIn, BusMuxInRz);
+							//Gen_Reg RZ(zRegIn, BusMuxInRz, Rzin, clear, clock);
 										
-							
-							alu aluu(ALUselect, clock, YData, bus, bus, ZReg, carry);
-						
+				
 
 							bus buss(R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out,
 										HIout, LOout, Zhighout, zlowout, Pout, MDRout, In_Portout, Cout, 
 										R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, 
 										HIin, LOin, Zhighin, zlowin,
-										Pin, MDRin, In_Portin, Cin);
+										Pin, MDRin, In_Portin, Cin, BusMuxOut);
+										
+							
+							alu aluu(ALUselect, clock, YData, BusMuxOut, Yin, ZReg, carry);
+							
+							RCAdder32 add(A, BusMuxOut, Cin);
 
 endmodule
--- Multiplication.v
+++ Multiplication.v
@@ -5,33 +5,33 @@ module Multiplicaion(
 
 reg signed [63:0] accumulator;
 reg [1:0] state;
-reg signed [31:0] A, S;
-reg signed [63:0] M;
+reg signed [31:0] M, S;
+reg signed [63:0] Q;
 integer i;
 wire clk;
 
 always @(posedge clk) begin
     case(state)
         2'b00: begin // Initial state
-            A = multiplicand;
-            S = -multiplicand; // 2's complement of A
-            M = {32'b0, multiplier}; // Pad multiplier with 32'b0
+            M = multiplicand;
+            S = -multiplicand; // 2's complement of M
+            Q = {32'b0, multiplier}; // Pad multiplier with 32'b0
             accumulator = 64'b0;
             state = 2'b01;
         end
         2'b01: begin // Booth encoding
-            if (M[1] == 1 && M[0] == 0) begin
-                accumulator = accumulator + A;
-                M = M + S;
-            end else if (M[1] == 0 && M[0] == 1) begin
+            if (Q[1] == 1 && Q[0] == 0) begin
                 accumulator = accumulator + S;
-                M = M + A;
+                Q = Q + S;
+            end else if (Q[1] == 0 && Q[0] == 1) begin
+                accumulator = accumulator + M;
+                Q = Q + M;
             end
             state = 2'b10;
         end
-        2'b10: begin // Right shift M and accumulator
+        2'b10: begin // Right shift Q and accumulator
             accumulator = accumulator >> 1;
-            M = M >> 1;
+            Q = Q >> 1;
             state = (i < 31) ? 2'b01 : 2'b11;
         end
         2'b11: begin // Final state
